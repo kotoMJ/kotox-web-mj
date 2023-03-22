c8b75fd952f6b9a8bc1972eb5de35a3af965e88a
Michal <jenicek.michal@gmail.com>
2023-03-22 20:05:51 +0100

    Fix wording.

--- _posts/aboutme.md
+++ _posts/aboutme.md
@@ -1,7 +1,7 @@
 ---
 title: 'Michal Jeníček • Mj'
-excerpt: '**I am** Michal (aka **MJ**)  - a **Software Engineer** coding in Central Bohemia of Czechia, Europe.
-I have been working for US startups as an **Android Engineer** over past 7   years. Besides that I am
+excerpt: '**I am** Michal (aka **MJ**)  - a **Software Engineer** crafting apps in Central Bohemia of Czechia, Europe.
+I am focused on **Android** craftsmanship over past 7 years. Besides that I am
 passionated in the full-stack development.'
 coverImage: '/assets/blog/about/cover-talk-brno.jpg'
 date: ''
@@ -13,9 +13,9 @@ ogImage:
     url: '/assets/blog/preview/cover.jpg'
 ---
 
-**I am** Michal (aka **MJ**)  - a **Software Engineer** coding in Central Bohemia of Czechia, Europe.
-I have been working for US startups as an **Android Engineer** over past 7 years. Besides that I am
+**I am** Michal (aka **MJ**)  - a **Software Engineer** crafting apps in Central Bohemia of Czechia, Europe.
+I am focused on **Android** craftsmanship over past 7 years. Besides that I am
 passionated in the full-stack development NodeJS, React, Express and MongoDB. Over and above
-that I have over 8 years of experience with Java Enterprise development, internet banking system mainly.
+that I have over 8 years of experience with Java Enterprise development, when developing internet banking system.
 
 ![myworkplace](/assets/blog/theme/cover-1.jpg)
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,54 +3,53 @@ import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
+import {getAllPosts} from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
 import Post from '../interfaces/post'
 
 type Props = {
-  allPosts: Post[]
+    allPosts: Post[]
 }
 
-export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout>
-        <Head>
-          <title>{`Android developer's blog`}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              metaData={heroPost.metaData}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
+export default function Index({allPosts}: Props) {
+    const heroPost = allPosts[0]
+    const morePosts = allPosts.slice(1)
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>{`Software craftsmanship by MJ`}</title>
+                </Head>
+                <Container>
+                    <Intro/>
+                    {heroPost && (
+                        <HeroPost
+                            title={heroPost.title}
+                            coverImage={heroPost.coverImage}
+                            date={heroPost.date}
+                            metaData={heroPost.metaData}
+                            slug={heroPost.slug}
+                            excerpt={heroPost.excerpt}
+                        />
+                    )}
+                    {morePosts.length > 0 && <MoreStories posts={morePosts}/>}
+                </Container>
+            </Layout>
+        </>
+    )
 }
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'metaData',
-    'coverImage',
-    'excerpt',
-  ])
+    const allPosts = getAllPosts([
+        'title',
+        'date',
+        'slug',
+        'metaData',
+        'coverImage',
+        'excerpt',
+    ])
 
-  return {
-    props: { allPosts },
-  }
+    return {
+        props: {allPosts},
+    }
 }

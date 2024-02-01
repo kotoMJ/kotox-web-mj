84f9376b004f3c1af905c562207f0b54032e1160
Michal <jenicek.michal@gmail.com>
2024-02-01 22:53:20 +0100

    Update dependencies

---
+++ _posts/cv.md
@@ -0,0 +1,16 @@
+---
+title: 'Michal Jeníček • Mj'
+excerpt: 'TBD. excerpt CV...'
+coverImage: ''
+date: ''
+metaData:
+    name: CV
+    picture: ''
+    tags: []
+ogImage:
+    url: ''
+ogTitle: "TBD. ogTitle CV..."
+root: true
+---
+
+TBD. CV here!
--- components/post-header.tsx
+++ components/post-header.tsx
@@ -18,9 +18,11 @@ const PostHeader = ({ title, coverImage, date, metaData }: Props) => {
       <div className="hidden md:block md:mb-12">
         <MetaDataView name={metaData.name} picture={metaData.picture} tags={metaData.tags} />
       </div>
-      <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} />
-      </div>
+        {coverImage.length>0 &&
+          <div className="mb-8 md:mb-16 sm:mx-0">
+            <CoverImage title={title} src={coverImage} />
+          </div>
+        }
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
           <MetaDataView name={metaData.name} picture={metaData.picture} tags={metaData.tags} />
